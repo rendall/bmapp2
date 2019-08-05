3725a36b19297874488e237156e83f81c77b5ff2
Rendall <rendall@gmail.com>
2019-08-05 12:45:19 +0300

    Attempt to build in Netlify

---
+++ functions/data.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const api_1 = require("../common/api");
+/** Returns the entire Burning Man data dump as a json object of the form
+ * { camps:[], art:[], events:[], timestamp:integer }
+ *
+ * @param year optional querystring (eg. ?year=2017) defaults to current year
+ */
+exports.handler = (event, context, callback) => {
+    const year = event.queryStringParameters && event.queryStringParameters["year"]
+        ? event.queryStringParameters["year"]
+        : new Date().getFullYear();
+    api_1.getData(year)
+        .then(body => callback(null, {
+        statusCode: 200,
+        body
+    }))
+        .catch((err) => callback({
+        message: err.statusMessage || "Unknown error",
+        name: `${err.statusCode}` || "500"
+    }, { statusCode: err.statusCode, body: err.statusMessage }));
+};
