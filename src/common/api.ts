0a5aab39c0a60d84402e9702fa04c8ba216b1138
Rendall <rendall@gmail.com>
2019-08-12 13:30:34 +0300

    Format document

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,52 +1,52 @@
-import * as dotenv from "dotenv";
-import * as http from "https";
-import { IncomingMessage } from "http";
-dotenv.config();
+import * as dotenv from "dotenv"
+import * as http from "https"
+import { IncomingMessage } from "http"
+dotenv.config()
 
-const API_KEY = process.env.API_KEY;
-const BM_URL = process.env.BM_URL;
-const basicAuth = { auth: `${API_KEY}:` };
+const API_KEY = process.env.API_KEY
+const BM_URL = process.env.BM_URL
+const basicAuth = { auth: `${API_KEY}:` }
 
 export type DataRetrievalError = {
-  statusCode: number | undefined;
-  statusMessage: string | undefined;
-  error : Error;
+  statusCode: number | undefined
+  statusMessage: string | undefined
+  error: Error
 }
 
 const dataPromise = (path: string) => (year: number | string) =>
   new Promise((resolve, reject) => {
     const ApiEndpoint = `${BM_URL}/${path}?year=${year}`
-    console.info("fetching", {path, ApiEndpoint, basicAuth, API_KEY});
+    console.info("fetching", { path, ApiEndpoint, basicAuth, API_KEY })
     http.get(
       ApiEndpoint,
       basicAuth,
       (res: IncomingMessage) => {
-        console.info(`receiving ${path}`);
+        console.info(`receiving ${path}`)
 
-        const { statusCode, statusMessage } = res;
+        const { statusCode, statusMessage } = res
 
-        console.info({ statusCode, statusMessage });
+        console.info({ statusCode, statusMessage })
 
-        res.on("error", (error) => { 
+        res.on("error", (error) => {
           res.resume()
           reject(JSON.stringify({ statusCode, statusMessage, error }))
         })
-        let dump = "";
-        res.on("data", data => (dump += data));
-        res.on("end", () => resolve(JSON.parse(dump)));
+        let dump = ""
+        res.on("data", data => (dump += data))
+        res.on("end", () => resolve(JSON.parse(dump)))
       }
-    );
-  });
+    )
+  })
 
-const campPromise = dataPromise("camp");
-const artPromise = dataPromise("art");
-const eventPromise = dataPromise("event");
+const campPromise = dataPromise("camp")
+const artPromise = dataPromise("art")
+const eventPromise = dataPromise("event")
 
 /** getData(year) returns a promise that yields the entire Burning Man 
  * data dump for <year> as a json object of the form 
  * { camps:[...], art:[...], events:[...], timestamp:integer }
  */
-export const getData = (year: number | string):Promise<string> =>
+export const getData = (year: number | string): Promise<string> =>
   Promise.all([campPromise(year), artPromise(year), eventPromise(year)])
     .then(v => ({
       camps: v[0],
@@ -54,4 +54,4 @@ export const getData = (year: number | string):Promise<string> =>
       events: v[2],
       timestamp: new Date().valueOf()
     }))
-    .then(data => JSON.stringify(data));
+    .then(data => JSON.stringify(data))
