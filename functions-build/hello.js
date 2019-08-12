f0db2115076d57df6145bab890830a4897d3fe77
Rendall <rendall@gmail.com>
2019-08-12 09:04:54 +0300

    Remove build directories

--- functions-build/hello.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = (event, context, callback) => {
-    callback(null, { statusCode: 200, body: "Hello world!" });
-};
